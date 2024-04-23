4862f30458147c7a6b48d870382901b41fe2b575
EagleNetwork <153021443+eaglenetwork@users.noreply.github.com>
2024-04-23 12:20:28 -0700

    ticker

---
+++ Assets/javascript.md
@@ -0,0 +1,6 @@
+# How to unblock running JS?
+1. Download [uBlock Origin](https://chromewebstore.google.com/detail/ublock-origin/cjpalhdlnbpafiamejdnhcphjbkeiagm)
+2. Click it in the extensions bar & press the gear for settings
+3. Tick "I am an advanced user" then press the gear next to it
+4. After ``UserResourcesLocation`` paste in ``https://eaglenetwork.github.io/uRun/urun.js`` instead of unset
+5. Press Ctrl + Shift + ` on most websites to start running javascript!
--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 function replace(link) {
     location.replace(link)
 }
+
+function cloak() {
+    var url = window.origin
+    var win = window.open();
+    win.document.body.style.margin = '0';
+    win.document.body.style.height = '100vh';
+    var iframe = win.document.createElement('iframe');
+    iframe.style.border = 'none';
+    iframe.style.width = '100%';
+    iframe.style.height = '100%';
+    iframe.style.margin = '0';
+    iframe.src = url;
+    win.document.body.appendChild(iframe);
+}
