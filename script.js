3125502b6c28c72d54b38b63b6e2ccb366e2ad21
EagleNetwork <153021443+eaglenetwork@users.noreply.github.com>
2024-04-23 14:01:33 -0700

    whaha

--- script.js
+++ script.js
@@ -1,39 +1,41 @@
-if (localStorage.getItem("autoCloak") == true) {
-    document.getElementById("autoCloak").value = "AutoCloak (On)"
-    document.getElementById("autoCloak").style.color = "blue"
-    if (window.location == window.parent.location) {
-        cloak()
-    }
-} else {
-    document.getElementById("autoCloak").value = "AutoCloak (Off)"
-    document.getElementById("autoCloak").style.color = "red"
-}
-
-function replace(link) {
-    location.replace(link)
-}
-
-function handleCloaker() {
-    if (localStorage.getItem("autoCloak") != true) {
-        localStorage.setItem("autoCloak", true)
-        alert("Enabled AutoCloaker. Press OK.")
-        cloak()
+setTimeout(function(){
+    if (localStorage.getItem("autoCloak") == true) {
+        document.getElementById("autoCloak").value = "AutoCloak (On)"
+        document.getElementById("autoCloak").style.color = "blue"
+        if (window.location == window.parent.location) {
+            cloak()
+        }
     } else {
-        localStorage.setItem("autoCloak", false)
+        document.getElementById("autoCloak").value = "AutoCloak (Off)"
+        document.getElementById("autoCloak").style.color = "red"
     }
-}
-
-function cloak() {
-    var url = "https://eaglenetwork.github.io/database"
-    var win = window.open();
-    win.document.body.style.margin = '0';
-    win.document.body.style.height = '100vh';
-    var iframe = win.document.createElement('iframe');
-    iframe.style.border = 'none';
-    iframe.style.width = '100%';
-    iframe.style.height = '100%';
-    iframe.style.margin = '0';
-    iframe.src = url;
-    win.document.body.appendChild(iframe);
-    location.replace("https://google.com")
-}
+    
+    function replace(link) {
+        location.replace(link)
+    }
+    
+    function handleCloaker() {
+        if (localStorage.getItem("autoCloak") != true) {
+            localStorage.setItem("autoCloak", true)
+            alert("Enabled AutoCloaker. Press OK.")
+            cloak()
+        } else {
+            localStorage.setItem("autoCloak", false)
+        }
+    }
+    
+    function cloak() {
+        var url = "https://eaglenetwork.github.io/database"
+        var win = window.open();
+        win.document.body.style.margin = '0';
+        win.document.body.style.height = '100vh';
+        var iframe = win.document.createElement('iframe');
+        iframe.style.border = 'none';
+        iframe.style.width = '100%';
+        iframe.style.height = '100%';
+        iframe.style.margin = '0';
+        iframe.src = url;
+        win.document.body.appendChild(iframe);
+        location.replace("https://google.com")
+    }
+},2000)
